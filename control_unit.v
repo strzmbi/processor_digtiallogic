8f14456189ba2fab2805a99ed512ba37d3ba9edf
Christine <149923541+strzmbi@users.noreply.github.com>
2026-05-05 11:40:37 +1000

    Control unit

---
+++ control_unit.v
@@ -0,0 +1,176 @@
+
+module control_unit ( 
+
+    input clk;
+    input [5:0] instruction;
+    input [4:0] argument_1, argument_2;
+
+    output reg program_counter_signal;
+    output reg result_register_signal;
+    output reg arithmetic_load_signal;
+    output reg memory_store_signal;
+
+    output reg [4:0] register_select;
+    output reg [5:0] instruction_select;
+
+);
+
+
+/***************************************************************************
+   STATES; 3 BIT BINARY ENCODINGS
+ -------------------------------------------------------------------------*/
+    parameter IDLE = 3'b000,
+              STORE = 3'b001,
+              STORE_REGISTER = 3'b010, 
+              LOAD_1 = 3'b011, 
+              LOAD_2 = 3'b100, 
+              LOAD_LITERAL = 3'b101, 
+              SEND_INSTRUCTION_SIGNALS = 3'b110, 
+              UPDATE_PC = 3'b111;
+
+/***************************************************************************
+   OPERATIONS
+ -------------------------------------------------------------------------*/
+    parameter ADD = 6'b000000,
+              SUB = 6'b000001,
+              LDI = 6'b000010,
+              INC = 6'b000011,
+              DEC = 6'b000100,
+              LSR = 6'b000101,
+              LSL = 6'b000110,
+              MOV = 6'b000111,
+              XOR = 6'b001000,
+              AND = 6'b001001,
+              NOR = 6'b001010,
+              NOT = 6'b001011,
+              OR = 6'b001100,
+              JE = 6'b001101,
+              JG = 6'b001110,
+              JL = 6'b001111,
+              JMP = 6'b010000,
+              LD = 6'b010001,
+              ST = 6'b010010,
+              CP = 6'b010011,
+        
+/***************************************************************************
+   INITIALISATION
+ -------------------------------------------------------------------------*/
+    reg [2:0] current_state = IDLE;
+    reg [2:0] next_state = IDLE;
+    reg [3:0] counter = 4'b0000;
+
+/***************************************************************************
+   SEQUENTIAL LOGIC
+ -------------------------------------------------------------------------*/
+    always @(posedge clk) begin
+        counter <= counter + 4'd1;
+        current_state <= next_state;
+    end
+
+/***************************************************************************
+   OUTPUT LOGIC
+ -------------------------------------------------------------------------*/
+    always @(posedge clk) begin
+        case (current_state)
+        //decode
+        IDLE: begin
+            case (instruction)
+                ADD, SUB, XOR, AND, XOR, NOR, OR, CP: next_state <= LOAD_2;
+                INC, DEC, LSL, LSR, NOT: next_state <= LOAD_1;
+                LDI: next_state <= LOAD_LITERAL;
+                default: next_state <= SEND_INSTRUCTION_SIGNALS;
+            endcase
+
+            counter <= 4'b0000;
+        end
+
+        //fetch
+        LOAD_1: begin
+            if (counter == 4'b0000) begin
+                register_select <= argument_1;
+                arithmetic_load_signal <= 1'd1;
+            end
+            else begin
+                arithmetic_load_signal <= 1'd0;
+                next_state <= SEND_INSTRUCTION_SIGNALS;
+                counter <= 4'b0000; 
+            end
+        end
+
+        LOAD_2: begin
+            if (counter == 4'b0000) begin
+                register_select <= argument_1;
+                arithmetic_load_signal <= 1'd1;         
+            end
+            else if (counter == 4'b0001) begin
+                arithmetic_load_signal <= 1'd0;
+                register_select <= argument_2;
+            end
+            else begin
+                next_state <= SEND_INSTRUCTION_SIGNALS;
+                counter <= 4'b0000; 
+            end
+        end
+
+        LOAD_LITERAL: begin
+        end
+
+        //execute
+        SEND_INSTRUCTION_SIGNALS: begin
+            case (instruction)
+                ADD, XOR, AND, XOR, NOR, OR, INC, DEC, LSL, NOT: begin
+                    instruction_select <= instruction;
+                    next_state <= STORE_REGISTER;
+                    counter <= 4'b0000; 
+                end
+                LSR: begin
+                    
+                end
+                CP, SUB: begin
+                    
+                end
+                JMP, JE, JG, JL: begin
+                    
+                end
+                LDI, LD, ST: begin
+                    
+                end
+            endcase
+        end
+
+        //store
+        STORE: begin
+            //program the storing mechanism here.
+
+            next_state <= UPDATE_PC;
+            counter <= 4'b0000;
+        end
+
+        STORE_REGISTER: begin
+            //program the storing mechanism here.
+
+            next_state <= UPDATE_PC;
+            counter <= 4'b0000;
+        end
+
+        UPDATE_PC: begin
+            if (counter == 0) begin
+                program_counter_signal <= 1'd1; 
+            end
+            else begin
+                program_counter_signal <= 1'd0;
+                next_state <= IDLE;
+                counter <= 4'b0000; 
+            end
+        end
+
+        default: next_state <= IDLE;
+        endcase
+    end
+
+endmodule
+
+/*
+If you are reading this, This video below has a great explaination of how this works:
+https://www.youtube.com/watch?v=Aq5WXmQQooo&pp=ygUJcmljayByb2xs
+*/
