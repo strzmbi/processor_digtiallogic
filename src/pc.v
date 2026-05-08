ccc5beb2679ccf404a6f808ebbc7547a1525d1dc
Christine Cui <christineevacui@gmail.com>
2026-05-08 15:15:30 +1000

    Program counter made

---
+++ src/pc.v
@@ -0,0 +1,27 @@
+primitive pc(
+
+    input clk,
+    input enable,
+    input branch_en,
+    input branch_add,
+    input reset,
+    output instruction
+
+);
+
+reg[31:0] pc = 32'b0;
+branch_en = 1'b0;
+
+always @(posedge clk) {
+    if (reset) begin
+        pc <= 32'b0;    /* reset to start address */ 
+    end else if (enable) begin
+            if (branch_en) begin
+                pc <= pc + branch_add;
+            end else begin
+                pc <= pc + 1; 
+            end
+        end 
+}
+
+endprimitive
