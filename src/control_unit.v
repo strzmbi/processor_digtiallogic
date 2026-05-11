e43050df3d49c0c401eca434dc25f0060791b371
Rudransh Kala <64187815+RogeDragon@users.noreply.github.com>
2026-05-11 22:47:22 +1000

    Refactor control unit with immediate loading and bus handling

--- src/control_unit.v
+++ src/control_unit.v
@@ -9,6 +9,11 @@ module control_unit (
     //Register Signal
     output reg [4:0]    register_tri;
     output reg [4:0]    register_en;
+
+	//Immediate Loading Signals 
+	output reg			immediate_en;
+	output reg   		immediate_tri;
+	output reg [15:0]	immediate_val;
     
     //Alu Signals
     output reg          alu_load_en;
@@ -62,23 +67,24 @@ module control_unit (
 
 
 /***************************************************************************
-   FSM STATES; 3 BIT BINARY ENCODINGS - up to 8 states
+   FSM STATES; 4 BIT BINARY ENCODINGS - up to 16 states
  -------------------------------------------------------------------------*/
  
-	localparam      IDLE                        = 3'b000,
-					LOAD_1                      = 3'b011, 
-					LOAD_2                      = 3'b100, 
-					UPDATE_PC                   = 3'b111,
-					LOAD_LITERAL                = 3'b101,
-					STORE_REGISTER              = 3'b010,
-					SEND_INSTRUCTION_SIGNALS    = 3'b110;
+	localparam      IDLE                        = 4'b0000,
+                    LOAD_BUS                    = 4'b0001,
+					LOAD_1                      = 4'b0010, 
+					LOAD_2                      = 4'b0011,
+					UPDATE_PC                   = 4'b0100,
+					LOAD_LITERAL                = 4'b0101,
+					STORE_REGISTER              = 4'b0110,
+					SEND_INSTRUCTION_SIGNALS    = 4'b0111;
 				
 /***************************************************************************
    SEQUENTIAL LOGIC
  -------------------------------------------------------------------------*/
  
 	reg [3:0]   clock_counter;
-	reg [2:0]   current_state,  next_state; 
+	reg [3:0]   current_state,  next_state; 
 	
     always @(posedge clk) begin
         clock_counter <= clock_counter + 4'd1;
@@ -96,6 +102,7 @@ module control_unit (
 					ADD, SUB, XOR, AND, XOR, NOR, OR, CP: next_state <= LOAD_2;
 					INC, DEC, LSL, LSR, NOT: next_state <= LOAD_1;
 					LDI: next_state <= LOAD_LITERAL;
+                    LD, ST: next_state <= LOAD_BUS;
 					
 					default: next_state <= SEND_INSTRUCTION_SIGNALS;
 				endcase
@@ -106,28 +113,28 @@ module control_unit (
 			LOAD_1: begin
 				if (clock_counter == 4'b0000) begin
 					
-					register_en <= argument_1;
+					register_tri <= argument_1;
 					alu_load_en <= 1'd1;
 				
 				end else begin
 				
 					alu_load_en <= 1'd0;
-					clock_counter <= 4'b0000;
 					next_state <= SEND_INSTRUCTION_SIGNALS;
-				
+                    clock_counter <= 4'b0000;
+
 				end
 			end
 			
 			LOAD_2: begin
 				if (clock_counter == 4'b0000) begin
 					
-					register_en <= argument_1;
+					register_tri <= argument_1;
 					alu_load_en <= 1'd1;
 				
 				end else if (clock_counter == 4'b0001) begin
 				
 					alu_load_en <= 1'd0;
-					register_en <= argument_2;
+					register_tri <= argument_2;
 				
 				end else begin
 				
@@ -137,18 +144,75 @@ module control_unit (
 				end
 			end
 			
-			LOAD_LITERAL: begin
-			
+			LOAD_LITERAL: begin // puts the value onto the bus
+				if (clock_counter == 4'b0000) begin
+					
+					immediate_val <= argument_1;
+					immediate_en <= 1'd1;
+					
+				end else if (clock_counter == 4'b0001) begin
+
+					immediate_en <= 1'd0;
+					immediate_tri <= 1'd1;
+					
+				end else begin 
+					
+					next_state <= SEND_INSTRUCTION_SIGNALS;
+					clock_counter <= 4'b0000; 
+
+				end
 			end
+
+            LOAD_BUS: begin
+                case (instruction)
+                    LD: begin // puts the memory onto the bus
+
+                        if (clock_counter == 4'b0000) begin // can break with register select
+                            
+                            memory_read_addr <= argument_2;
+                            memory_read_en <= 1'd1;
+                                                        
+                        end else begin
+                            memory_read_en <= 1'd0;
+
+                            next_state <= SEND_INSTRUCTION_SIGNALS;
+                            clock_counter <= 4'b0000;
+                        end
+                        
+                    end 
+                    default: begin // puts register onto the bus
+
+                        if (clock_counter == 4'b0000) begin // can break with register select
+
+                            memory_write_addr <= argument_2;
+                            register_tri <= argument_1;
+                            memory_write_en <= 1'd1;
+                            
+                        end else begin
+                            memory_write_en <= 1'd0;
+
+                            next_state <= SEND_INSTRUCTION_SIGNALS;
+                            clock_counter <= 4'b0000;
+                        end
+
+                    end
+                endcase
+            end
 			
 			SEND_INSTRUCTION_SIGNALS: begin
-			
+                case (instruction)
+                    
+                endcase
 			end
 			
 			STORE_REGISTER: begin
-				//program the storing mechanism here!
-				next_state <= UPDATE_PC;
-				clock_counter <= 4'b0000;
+                if (clock_counter == 0) begin
+                    register_tri <= argument_1;
+				end
+				else begin
+					next_state <= UPDATE_PC;
+					clock_counter <= 4'b0000; 
+				end
 			end
 			
 			UPDATE_PC: begin
