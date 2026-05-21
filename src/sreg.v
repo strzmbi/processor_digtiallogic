41d71ef2eeac4b5edb6cf76b7ba6cd04ec9b6bf1
Rudransh Kala <64187815+RogeDragon@users.noreply.github.com>
2026-05-21 13:07:38 +1000

    Add status register module for comparison logic

---
+++ src/sreg.v
@@ -0,0 +1,37 @@
+
+
+module sreg(
+	//Status Register Input
+	input [15:0]  		difference,
+	input 		  		clk,
+	input		  		rst,
+	input			  	write_e,
+	
+	//Status Register Output
+	output reg [2:0]  	values);
+	
+/***************************************************************************
+   Status Register Output Bits
+ -------------------------------------------------------------------------
+   LSB		GREATER
+   BIT 2	LESS
+   MSB		EQUAL	
+ --------------------------------------------------------------------------*/
+
+    always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			values <= 16'd0;
+		end else begin
+			if (difference > 16'd0 && difference < 16'h8000) begin
+				values <= 3'b001;
+			end
+			else if (difference > 16'h8000) begin
+				values <= 3'bb010;
+			end
+			else if (difference == 16'd0)   begin
+				values <= 3'bb100;
+			end
+		end
+    end
+	
+endmodule
