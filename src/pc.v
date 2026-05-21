0b0ff9e604898d1b8ee2b666e42d9bd151494477
Rudransh Kala <64187815+RogeDragon@users.noreply.github.com>
2026-05-21 13:11:47 +1000

    Refactor pc module with branching support

--- src/pc.v
+++ src/pc.v
@@ -1,27 +1,22 @@
-primitive pc(
-
-    input clk,
-    input enable,
-    input branch_en,
-    input branch_add,
-    input reset,
-    output instruction
-
+module pc(
+    input clk,              // clock input
+    input enable,           // enable for counter
+    input branch_en,        // branching enabler
+    input[31:0] branch_addr, // branch address
+    input reset,            // reset input
+    output reg[4:0] pc     
 );
 
-reg[31:0] pc = 32'b0;
-branch_en = 1'b0;
-
-always @(posedge clk) {
+always @(posedge clk or posedge reset) begin
     if (reset) begin
-        pc <= 32'b0;    /* reset to start address */ 
+        pc <= 32'b0;            /* reset to start address */ 
     end else if (enable) begin
             if (branch_en) begin
-                pc <= pc + branch_add;
+                pc <= branch_addr;
             end else begin
                 pc <= pc + 1; 
             end
         end 
-}
+end
 
-endprimitive
+endmodule
